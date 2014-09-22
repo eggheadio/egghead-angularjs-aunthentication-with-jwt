6784f2886458a2840a8b2530cf28692e947f477c
Joel Hooks <joelhooks@gmail.com>
2014-09-22 11:44:56 -0500

    auth interceptor

--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
 (function () {
   'use strict';
-  var app = angular.module('app', []);
+  var app = angular.module('app', [], function config($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  });
 
   app.constant('API_URL', 'http://localhost:3000');
 
@@ -9,6 +11,7 @@
     var vm = this;
     vm.getRandomUser = getRandomUser;
     vm.login = login;
+    vm.logout = logout;
 
     function getRandomUser() {
       RandomUserFactory.getUser().then(function success(response) {
@@ -20,6 +23,11 @@
       UserFactory.login(username, password).then(function success(response) {
         vm.user = response.data.user;
       }, handleError);
+    }
+
+    function logout() {
+      UserFactory.logout();
+      vm.user = null;
     }
 
     function handleError(response) {
@@ -38,17 +46,65 @@
     }
   });
 
-  app.factory('UserFactory', function UserFactory($http, API_URL) {
+  app.factory('UserFactory', function UserFactory($http, API_URL, AuthTokenFactory) {
     'use strict';
     return {
-      login: login
+      login: login,
+      logout: logout
     };
 
     function login(username, password) {
       return $http.post(API_URL + '/login', {
         username: username,
         password: password
+      }).then(function success(response) {
+        AuthTokenFactory.setToken(response.data.token);
+        return response;
       });
+    }
+
+    function logout() {
+      AuthTokenFactory.setToken();
+    }
+  });
+
+  app.factory('AuthTokenFactory', function AuthTokenFactory($window) {
+    'use strict';
+    var store = $window.localStorage;
+    var key = 'auth-token';
+
+    return {
+      getToken: getToken,
+      setToken: setToken
+    };
+
+    function getToken() {
+      return store.getItem(key);
+    }
+
+    function setToken(token) {
+      if (token) {
+        store.setItem(key, token);
+      } else {
+        store.removeItem(key);
+      }
+    }
+
+  });
+
+  app.factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
+    'use strict';
+    return {
+      request: addToken
+    };
+
+    function addToken(config) {
+      var token = AuthTokenFactory.getToken();
+      if (token) {
+        config.headers = config.headers || {};
+        config.headers.Authorization = 'Bearer ' + token;
+      }
+      return config;
     }
   });
 })();
