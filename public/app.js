0389c558b5715793b896c4e8935219c33b0d6585
Joel Hooks <joelhooks@gmail.com>
2014-09-22 11:41:38 -0500

    authentication setup

--- public/app.js
+++ public/app.js
@@ -4,15 +4,26 @@
 
   app.constant('API_URL', 'http://localhost:3000');
 
-  app.controller('MainCtrl', function MainCtrl(RandomUserFactory) {
+  app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory) {
     'use strict';
     var vm = this;
     vm.getRandomUser = getRandomUser;
+    vm.login = login;
 
     function getRandomUser() {
       RandomUserFactory.getUser().then(function success(response) {
         vm.randomUser = response.data;
-      });
+      }, handleError);
+    }
+
+    function login(username, password) {
+      UserFactory.login(username, password).then(function success(response) {
+        vm.user = response.data;
+      }, handleError);
+    }
+
+    function handleError(response) {
+      alert('Error: ' + response.data);
     }
   });
 
@@ -24,6 +35,20 @@
 
     function getUser() {
       return $http.get(API_URL + '/random-user');
+    }
+  });
+
+  app.factory('UserFactory', function UserFactory($http, API_URL) {
+    'use strict';
+    return {
+      login: login
+    };
+
+    function login(username, password) {
+      return $http.post(API_URL + '/login', {
+        username: username,
+        password: password
+      });
     }
   });
 })();
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var user = {
+  username: 'kentcdodds',
+  password: 'p'
+};
 
 var app = express();
 
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/random-user', function (req, res) {
   var user = faker.Helpers.userCard();
@@ -13,6 +20,23 @@ app.get('/random-user', function (req, res) {
   res.json(user);
 });
 
+app.post('/login', authenticate, function (req, res) {
+  res.send(user);
+});
+
 app.listen(3000, function () {
   console.log('App listening on localhost:3000');
 });
+
+// UTIL FUNCTIONS
+
+function authenticate(req, res, next) {
+  var body = req.body;
+  if (!body.username || !body.password) {
+    res.status(400).end('Must provide username or password');
+  }
+  if (body.username !== user.username || body.password !== user.password) {
+    res.status(401).end('Username or password incorrect');
+  }
+  next();
+}
